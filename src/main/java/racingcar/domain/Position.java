73fae38dc288f0d1090f6158a2b74828dca590f7
Junseo <air.junseo@gmail.com>
2021-02-11 10:48:33 +0900

    refactor: 불변객체 Position에 equals & hashCode 재정의

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Position implements Comparable<Position> {
     private static final int NEXT = 1;
     private final int position;
@@ -23,5 +25,18 @@ public class Position implements Comparable<Position> {
 
     public int getPosition() {
         return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
